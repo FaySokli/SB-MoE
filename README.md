1d559d82372e4e1834aba4e6f77b4606507e60e7
FaySokli <efsokli@gmail.com>
2025-11-26 10:50:31 +0000

    add requirements.txt

--- README.md
+++ README.md
@@ -3,14 +3,19 @@ Read the paper [here](https://arxiv.org/abs/2510.15683). <br>
 To reproduce the experiments, complete the following steps:
 
 ## Step 1:
+Create a virtual environment and run the following: <br>
+```pip install -r requirements.txt``` <br><br>
+to install all necessary requirements.
+
+## Step 2:
 Run the following command to complete data pre-processing and bring all datasets to the BEIR format: <br>
 ```python3 data_preprocessing.py``` <br><br>
 For our experiments, we use the following four publicly available IR benchmarks: (i) HotpotQA and (ii) Natural Questions from the [BEIR collection](https://arxiv.org/abs/2104.08663), as well as the (iii) Political Science and (iv) Computer Science collections from the [Multi-Domain Benchmark](https://dl.acm.org/doi/abs/10.1145/3511808.3557536). We bring the last two collections to the BEIR format for our experiments.
 
-## Step 2:
+## Step 3:
 Update the parameters in the ```pipeline.sh``` file to the desired ones and execute the script. <br><br>
 For example, the following command will train *sbmoe_ALL* on TinyBERT using the HotpotQA dataset by employing 6 experts: <br>
-```python3 1_train_new_moe.py model=tinybert dataset=hotpotqa testing=hotpotqa model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all``` <br><br>
+```python3 1_train_new_moe.py model=tinybert dataset=hotpotqa testing=hotpotqa model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all``` <br><br>
 _*Note: When use_adapters=True, only two modes are allowed; (i) 'sbmoe_all' and (ii) 'sbmoe_top1'.
 
 ## 3-Dimensional T-SNE plots
--- src/pipeline.sh
+++ src/pipeline.sh
@@ -1,39 +1,39 @@
 DATASET=computer_science
 MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 
 
 DATASET=political_science
 MODEL=contriever-base
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 
 
 DATASET=hotpotqa
 MODEL=bert-base
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_all
 
 
 DATASET=nq-train
 MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts=6 model.init.specialized_mode=sbmoe_top1
 
